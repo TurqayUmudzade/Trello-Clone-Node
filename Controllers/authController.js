ddcb41d92299740498bb2790a10bfb063c3fde05
Turqay Umudzade <turgayau@code.edu.az>
2020-08-27 05:41:40 +0400

    logout added

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -74,3 +74,9 @@ module.exports.login_post = async(req, res) => {
         res.status(400).json({ errors })
     }
 }
+
+
+module.exports.logout_get = async(req, res) => {
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/');
+}
--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const authController = require('../Controllers/authController');
 
 
-router.get('/signup', authController.signup_get)
-router.post('/signup', authController.signup_post)
-router.get('/login', authController.login_get)
-router.post('/login', authController.login_post)
+router.get('/signup', authController.signup_get);
+router.post('/signup', authController.signup_post);
+
+router.get('/login', authController.login_get);
+router.post('/login', authController.login_post);
+
+router.get('/logout', authController.logout_get);
 
 
 module.exports = router;
