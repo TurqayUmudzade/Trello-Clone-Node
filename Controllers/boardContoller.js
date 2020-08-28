7b978cfe0763cb68bee6126b0c4e77732a36993c
Turqay Umudzade <turgayau@code.edu.az>
2020-08-28 05:39:19 +0400

    blog/id created

--- Controllers/boardContoller.js
+++ Controllers/boardContoller.js
@@ -2,17 +2,28 @@ const { request } = require("express");
 
 const Board = require('../Models/Board');
 
+//my-board
 module.exports.myBoards = (req, res) => {
     Board.find().then(result => {
-        console.log(result);
         res.render('my-boards', { boards: result })
     }).catch(err => {
         console.log(err);
     });
 }
 
+//POST
 module.exports.CreateBoard = async(req, res) => {
     const { name } = req.body;
     await Board.create({ name });
     res.redirect('/')
+}
+
+//my-board/id
+module.exports.BoardDetails = async(req, res) => {
+    const id = req.params.id;
+    Board.findById(id).then(board => {
+        res.render('board-details', { board: board })
+    }).catch(err => {
+        console.log(err);
+    })
 }
--- Routes/boardRoutes.js
+++ Routes/boardRoutes.js
@@ -4,5 +4,6 @@ const boardContoller = require('../Controllers/boardContoller');
 
 router.get('/my-boards', boardContoller.myBoards)
 router.post('/create-board', boardContoller.CreateBoard)
+router.get('/my-boards/:id', boardContoller.BoardDetails)
 
 module.exports = router;
