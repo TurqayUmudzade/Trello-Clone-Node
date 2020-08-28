2bbe6311dabc981dab1ff885cb0c1e5afee57575
Turqay Umudzade <turgayau@code.edu.az>
2020-08-28 06:01:48 +0400

    created board-list relational model

--- Models/Board.js
+++ Models/Board.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
+const listSchema = mongoose.Schema({
+    header: String,
+    listItems: [String]
+})
+
 const BoardSchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
-    }
+    },
+    lists: [listSchema]
 })
 
 const Board = mongoose.model('board', BoardSchema);
