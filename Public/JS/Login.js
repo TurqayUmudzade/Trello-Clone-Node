44f3023903ade10f39f8feea9b5be0370e83d653
Turqay Umudzade <turgayau@code.edu.az>
2020-08-31 05:27:35 +0400

    updated register/login views

---
+++ Public/JS/Login.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('form');
+const emailError = document.querySelector('#email-validation');
+const passwordError = document.querySelector('#password-validation');
+form.addEventListener('submit', async(e) => {
+
+    e.preventDefault();
+
+    emailError.textContent = ' ';
+    passwordError.textContent = ' ';
+    const email = form.email.value;
+    const password = form.password.value;
+
+    try {
+        const res = await fetch('/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                email,
+                password
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        //return errors
+        const data = await res.json();
+
+        if (data.errors) {
+            emailError.textContent = data.errors.email;
+            passwordError.textContent = data.errors.password;
+        }
+        if (data.user) {
+            location.assign('/')
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+
+})
